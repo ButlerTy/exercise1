04e39d3fd657923db4cc920d7ee72819f218ed97
ButlerTy <60446965+ButlerTy@users.noreply.github.com>
2024-02-02 15:24:05 -0500

    Updated Printinfo

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -69,19 +69,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Ty");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- Get a fullfilling job that i enjoy");
+        System.out.println("-- Get paid");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Video Games");
+        System.out.println("-- Sports");
+        System.out.println("-- Watching TV");
+        System.out.println("-- Programming");
 
         System.out.println();
         
